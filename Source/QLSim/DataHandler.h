afab78e1d0b20ff7e4568594a7b2f1306dd95a4c
Sean Pearce <72108425+spear97@users.noreply.github.com>
2024-02-26 09:28:08 -0600

    Fixed another bug causing compilation errors

---
+++ Source/QLSim/DataHandler.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "DataHandler.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QLSIM_API UDataHandler : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
